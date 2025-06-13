9c80d0e8d5d42e0a5e63d7421152617e227dd95d
cnk developer <cnkdev@cnkonline.in>
2025-06-13 18:11:59 +0530

    Commit before weekend.

--- scratchpad.py
+++ scratchpad.py
@@ -1,8 +1,9 @@
-# Number gussing game
+# Number guessing game
 import random as rd 
-print("Welcome! to the Number gussing game.")
+print("Welcome! to the Number guessing game.")
 low = int(input("low limit: "))
 high = int(input("high limit: "))
 
 x = rd.randint(low, high)
 print(x)
+print(x +3)
